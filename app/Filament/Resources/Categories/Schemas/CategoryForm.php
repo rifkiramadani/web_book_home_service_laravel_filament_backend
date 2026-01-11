81d5c97fcd220dc6ff1ee60e45f6ee32c7f3343e
Muhammad Rifky Ramadani <rifkiramadani724@gmail.com>
2026-01-11 22:34:15 +0700

    feat: make category resource

--- app/Filament/Resources/Categories/Schemas/CategoryForm.php
+++ app/Filament/Resources/Categories/Schemas/CategoryForm.php
@@ -3,6 +3,8 @@
 namespace App\Filament\Resources\Categories\Schemas;
 
 use Filament\Schemas\Schema;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\FileUpload;
 
 class CategoryForm
 {
@@ -10,7 +12,19 @@ class CategoryForm
     {
         return $schema
             ->components([
-                //
+                TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                FileUpload::make('photo')
+                    ->required()
+                    ->image()
+                    ->directory('categories_photo')
+                    ->disk('public'),
+                FileUpload::make('photo_white')
+                    ->required()
+                    ->image()
+                    ->directory('categories_photo_white')
+                    ->disk('public')
             ]);
     }
 }
--- app/Filament/Resources/Categories/Tables/CategoriesTable.php
+++ app/Filament/Resources/Categories/Tables/CategoriesTable.php
@@ -5,10 +5,14 @@ namespace App\Filament\Resources\Categories\Tables;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
+use Filament\Actions\DeleteAction;
 use Filament\Actions\ForceDeleteBulkAction;
 use Filament\Actions\RestoreBulkAction;
 use Filament\Tables\Filters\TrashedFilter;
 use Filament\Tables\Table;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ImageColumn;
 
 class CategoriesTable
 {
@@ -16,13 +20,22 @@ class CategoriesTable
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('name')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('slug'),
+                ImageColumn::make('photo')
+                    ->disk('public'),
+                ImageColumn::make('photo_white')
+                    ->disk('public'),
             ])
             ->filters([
                 TrashedFilter::make(),
             ])
             ->recordActions([
+                ViewAction::make(),
                 EditAction::make(),
+                DeleteAction::make(),
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
