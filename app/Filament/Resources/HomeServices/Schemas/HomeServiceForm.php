df3c49e3c72063d6b93d2f95d012df7744f889bd
Muhammad Rifky Ramadani <rifkiramadani724@gmail.com>
2026-01-11 23:23:53 +0700

    feat: make home service resource

--- app/Filament/Resources/HomeServices/Schemas/HomeServiceForm.php
+++ app/Filament/Resources/HomeServices/Schemas/HomeServiceForm.php
@@ -3,6 +3,12 @@
 namespace App\Filament\Resources\HomeServices\Schemas;
 
 use Filament\Schemas\Schema;
+use Filament\Schemas\Components\Fieldset;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\Repeater;
 
 class HomeServiceForm
 {
@@ -10,7 +16,52 @@ class HomeServiceForm
     {
         return $schema
             ->components([
-                //
+                Fieldset::make('General Information')
+                    ->columnSpan(2)
+                    ->schema([
+                        Textinput::make('name')
+                            ->required()
+                            ->maxLength(255),
+                        FileUpload::make('thumbnail')
+                            ->required()
+                            ->image()
+                            ->directory('home_services_thumbnail')
+                            ->disk('public'),
+                        Select::make('category_id')
+                            ->relationship('category', 'name')
+                            ->required()
+                            ->preload()
+                            ->searchable(),
+                        TextInput::make('price')
+                            ->required()
+                            ->numeric()
+                            ->prefix('IDR')
+
+                    ]),
+                Fieldset::make('Additional Information')
+                    ->columnSpan(2)
+                    ->schema([
+                        Textarea::make('about')
+                            ->required(),
+                        Select::make('is_popular')
+                            ->label('Popular?')
+                            ->required()
+                            ->options([
+                                true => 'Popular',
+                                false => 'Not Popular'
+                            ]),
+                        TextInput::make('duration')
+                            ->required()
+                            ->numeric()
+                            ->prefix('Hours'),
+                        Repeater::make('Benefits')
+                            ->relationship('serviceBenefits')
+                            ->schema([
+                                TextInput::make('name')
+                                    ->required()
+                                    ->maxLength(255)
+                            ])
+                    ])
             ]);
     }
 }
--- app/Filament/Resources/HomeServices/Tables/HomeServicesTable.php
+++ app/Filament/Resources/HomeServices/Tables/HomeServicesTable.php
@@ -5,10 +5,17 @@ namespace App\Filament\Resources\HomeServices\Tables;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
+use Filament\Actions\DeleteAction;
 use Filament\Actions\ForceDeleteBulkAction;
 use Filament\Actions\RestoreBulkAction;
 use Filament\Tables\Filters\TrashedFilter;
 use Filament\Tables\Table;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Filters\SelectFilter;
 
 class HomeServicesTable
 {
@@ -17,12 +24,31 @@ class HomeServicesTable
         return $table
             ->columns([
                 //
+                //thumbnail, name, category, is_popular
+                ImageColumn::make('thumbnail')
+                    ->disk('public'),
+                TextColumn::make('name')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('category.name'),
+                IconColumn::make('is_popular')
+                    ->boolean()
+                    ->trueColor('success')
+                    ->falseColor('danger')
+                    ->trueIcon(Heroicon::OutlinedCheckCircle)
+                    ->falseIcon(Heroicon::OutlinedXCircle)
+                    ->label('Popular?')
             ])
             ->filters([
                 TrashedFilter::make(),
+                SelectFilter::make('category_id')
+                    ->relationship('category', 'name')
+                    ->label('Category')
             ])
             ->recordActions([
+                ViewAction::make(),
                 EditAction::make(),
+                DeleteAction::make(),
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
