8de32d45f500838aa21ea92dec1e693c44bdeffe
Muhammad Rifky Ramadani <rifkiramadani724@gmail.com>
2026-01-14 00:00:17 +0700

    feat: make Booking Transaction Controller logic

--- app/Http/Controllers/Api/BookingTransactionController.php
+++ app/Http/Controllers/Api/BookingTransactionController.php
@@ -2,10 +2,88 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use Carbon\Carbon;
+use App\Models\HomeService;
 use Illuminate\Http\Request;
+use App\Models\BookingTransaction;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use Illuminate\Validation\ValidationException;
+use App\Http\Requests\StoreBookingTransactionRequest;
 
 class BookingTransactionController extends Controller
 {
     //
+    public function store(StoreBookingTransactionRequest $request) {
+        //validasi request
+        $validated = $request->validate();
+
+        DB::beginTransaction();
+
+        try {
+            //handle file upload
+            if($request->hasFile('proof')) {
+                $path = $request->file('proof')->store('payment_proof', 'public');
+                $validated['proof'] = $path;
+            }
+
+            // VALIDASI DATA SEBELUM DI STORE
+            //ambil data array ids dari request
+            $serviceids = $request->input('service_ids');
+
+            //jika service tidak ada, maka tampilkan pesan error
+            if(empty($serviceids)) {
+                return response()->json(['message' => 'No Service Selected'], 400);
+            }
+
+            $services = HomeService::whereIn('id', $serviceids)->get(); //collection dari HomeService yang dipilih dari request array;
+
+            //jika collection services dari request tidak ada maka tampilkan pesan error
+            if($services->isEmpty()) {
+                return response()->json(['message' => 'Invalid services'], 400);
+            }
+
+            //kalkulasikan total price, tax sehingga mendapatkan grand total
+            $totalPrice = $services->sum('price');
+            $tax = 0.11 * $totalPrice;
+            $grandTotal = $totalPrice + $tax;
+
+            //set schedule_at menjadi keesokan hari nya atau tommorow
+            $validated['schedule_at'] = Carbon::tommorow()->toDateString(); //parse menjadi date string
+
+            //set beberapa data transaction payment
+            $validated['total_amount'] = $grandTotal;
+            $validated['sub_total'] = $totalPrice;
+            $validated['total_tax_amount'] = $tax;
+            $validated['is_paid'] = false;
+            $validated['booking_trx_id'] = BookingTransaction::generateUniqueTrxId();
+
+            //JIKA SEMUA DATA SUDAH LOLOS VALIDASI MAKA DATA SIAP DI STORE
+            $newBookingTransaction = BookingTransaction::create($validated); //STORE DATA
+            DB::commit();
+
+            //jika ada data $newBookingTransaction gagal di store maka tampilkan pesan error
+            if(!$newBookingTransaction) {
+                DB::rollback();
+                return response()->json(['message' => 'booking transaction not created'], 500);
+            }
+
+            //simpan atau store untuk setiap data service pada table transaction details
+            foreach($services as $service) {
+                $newBookingTransaction->transactionDetails()->create([
+                    'home_service_id' => $service->id,
+                    'price' => $service->price,
+                ]);
+            }
+
+            //setelah semua data di store atau di simpan maka kembalikan nilai tersebut agar bisa di tampilkan di halaman booking success/finished
+            return new BookingTransactionApiResource($newBookingTransaction->load(['transactionDetails']));
+            // return response()->json(['message' => 'booking transaction has been created'], 200);
+
+        } catch (\ValidationException $e) {
+            DB::rollback();
+            return response()->json(['message' => 'an error occured', 'error' => $e->getMessage()], 500);
+        }
+
+    }
 }
---
+++ app/Http/Requests/StoreBookingTransactionRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreBookingTransactionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+            'name' => 'required|string|max:255',
+            'phone' => 'required|string|max:255',
+            'email' => 'required|string|max:255',
+            'city' => 'required|string|max:255',
+            'address' => 'required|string|max:255',
+            'post_code' => 'required|string|max:255',
+            'started_time' => 'required|date_format:H:i',
+            'proof' => 'required|image|mimes:png,jpg,jpeg|max:2048',
+            'home_service_ids' => 'required|array', //memvalidasi array
+            'home_service_ids.*' => 'integer|exists:home_services,id', //memvalidasi per item
+        ];
+    }
+}
