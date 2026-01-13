710a82d7409461972f9001bd80886e496589e27f
Muhammad Rifky Ramadani <rifkiramadani724@gmail.com>
2026-01-13 23:15:43 +0700

    feat: make Category Controller logic

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -7,5 +7,19 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function index(Request $request) {
+        $categories = Category::query();
+
+        if($request->has('limit')) {
+            $categories->limit($request->input('limit'));
+        }
+
+        return CategoryApiResource::collection($categories->get());
+    }
+
+    public function shoW(Category $category) {
+        $category->load(['homeServices']);
+
+        return new CategoryApiResource($category);
+    }
 }
