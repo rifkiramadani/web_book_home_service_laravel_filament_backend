00a2c32a15958f19b4e4eed3f90828f5c6e86174
Muhammad Rifky Ramadani <rifkiramadani724@gmail.com>
2026-01-13 23:09:27 +0700

    feat: make HomeService Controller logic

--- app/Http/Controllers/Api/HomeServiceController.php
+++ app/Http/Controllers/Api/HomeServiceController.php
@@ -2,10 +2,38 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Models\HomeService;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class HomeServiceController extends Controller
 {
-    //
+    public function index(Request $request) {
+        //with untuk ambil relasi langsung dari model (eager loading)
+        $homeServices = HomeService::with(['category']);
+
+        if($request->has('category_id')) {
+            $homeServices->where('category_id', $request->input('category_id'));
+        }
+
+        if($request->has('is_popular')) {
+            $homeServices->where('is_popular', $request->input('is_popular'));
+        }
+
+        if($request->has('limit')) {
+            $homeServices->limit($request->input('limit'));
+        }
+
+        //mengembalikan nilai berupa collection
+        return HomeServiceApiResource::collection($homeServices->get());
+    }
+
+    public function show(HomeService $homeService) {
+        //load untuk model binding (eager loading)
+        $homeService->load(['category', 'serviceBenefits', 'serviceTestimonials']);
+
+        return new HomeServiceApiResource($homeService);
+    }
+
+
 }
